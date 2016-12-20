584d80066c362069c6da41220bad7c1389fa2b22
zxclovetotti <zxclovetotti@gmail.com>
2016-12-20 22:51:06 +0800

    fix warning

---
+++ src/js/highlightRows.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Leo on 16/12/20.
+ */
+function highlightRows(){
+    if (!document.getElementsByTagName) return false;
+
+    var rows = document.getElementsByTagName("tr");
+
+    for (var i = 0; i < rows.length; i++){
+        rows[i].onmouseover = function () {
+            this.style.fontWeight = 'bold';
+        };
+        rows[i].onmouseout = function () {
+            this.style.fontWeight = 'normal';
+        };
+    }
+}
+
+addLoadEvent(highlightRows);
