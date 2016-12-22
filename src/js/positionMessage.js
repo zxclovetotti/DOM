13fc8bf04ad378c7532d4fc830078a748c2a976b
zxclovetotti <zxclovetotti@gmail.com>
2016-12-22 22:02:27 +0800

    improve animation demo

--- src/js/positionMessage.js
+++ src/js/positionMessage.js
@@ -8,6 +8,13 @@ function positionMessage() {
     elem.style.position = "absolute";
     elem.style.left = "50px";
     elem.style.top = "100px";
-    moveElement("message",200,100,10);
+    moveElement("message",125,25,20);
+
+    if (!document.getElementById("message2")) return false;
+    var elem = document.getElementById("message2");
+    elem.style.position = "absolute";
+    elem.style.left = "50px";
+    elem.style.top = "50px";
+    moveElement("message2",125,125,20);
 }
 addLoadEvent(positionMessage);
