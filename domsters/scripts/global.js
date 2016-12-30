8fffa1d90f0301bdf01364a5cf61ae03e43f0237
zxclovetotti <zxclovetotti@gmail.com>
2016-12-30 19:46:46 +0800

    added showPic

--- domsters/scripts/global.js
+++ domsters/scripts/global.js
@@ -206,7 +206,91 @@ function  prepareInternalnav() {
     }
 }
 
+function showPic(whichpic){
+    if (!document.getElementById("placeholder")) return false;
+
+    //DOM-Core
+    //var source = whichpic.getAttribute("href");
+
+    //HTML-DOM
+    var source = whichpic.href;
+
+
+    var placeholder = document.getElementById("placeholder");
+
+    if (placeholder.nodeName != "IMG") return false;
+
+    //DOM-Core
+    //placeholder.setAttribute("src", source);
+
+    //HTML-DOM
+    placeholder.src = source;
+
+    /*
+     There are twelve possible values for nodeType, but only three of them are going to be of much
+     practical use:
+     • Element nodes have a nodeType value of 1.
+     • Attribute nodes have a nodeType value of 2.
+     • Text nodes have a nodeType value of 3.
+     This means that you can target specific types of nodes in your functions. For instance, you could
+     create a function that only affects element nodes.
+     */
+
+
+    if (document.getElementById("description")){
+        var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
+        var description = document.getElementById("description");
+
+        if (description.firstChild.nodeType == 3){
+            description.firstChild.nodeValue = text;
+        }
+    }
+    return true;
+}
+
+
+function preparePlaceholder() {
+    if (!document.createElement) return false;
+    if (!document.createTextNode) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id","placeholder");
+    placeholder.setAttribute("src","images/coffee.JPG");
+    placeholder.setAttribute("alt","my image gallery");
+
+    var description = document.createElement("p");
+    description.setAttribute("id","description");
+
+    var desctxt = document.createTextNode("Choose an image");
+    description.appendChild(desctxt);
+
+    var gallery = document.getElementById("imagegallery");
+    insertAfter(description,gallery);
+    insertAfter(placeholder,description);
+}
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+
+    for (var i=0; i < links.length; i++) {
+        links[i].onclick = function() {
+            return showPic(this) ? false : true;
+        }
+
+        //links[i].onkeypress = links[i].onclick;
+    }
+}
+
 // Load events
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
 addLoadEvent(prepareInternalnav);
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
