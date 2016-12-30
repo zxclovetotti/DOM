b62eb15f993490ba83f6dcc5538728300f4394b1
zxclovetotti <zxclovetotti@gmail.com>
2016-12-30 17:54:19 +0800

    added showSection function

--- domsters/scripts/global.js
+++ domsters/scripts/global.js
@@ -174,7 +174,7 @@ function prepareSlideshow(){
 }
 
 function showSection(id) {
-    var sections = document.getElementsByName("section");
+    var sections = document.getElementsByTagName("section");
     for (var i = 0; i < sections.length; i++){
         if (sections[i].getAttribute("id") != id){
             sections[i].style.display = "none";
@@ -194,7 +194,7 @@ function  prepareInternalnav() {
     var nav = navs[0];
     var links = nav.getElementsByTagName("a");
     for (var i = 0; i < links.length; i++ ){
-        var sectionId = links[i].getAttribute("href").split("#")[i];
+        var sectionId = links[i].getAttribute("href").split("#")[1];
         if (!document.getElementById(sectionId)) continue;
 
         document.getElementById(sectionId).style.display = "none";
