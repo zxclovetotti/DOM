e76fd006e188e29bf25b1962c450baa3c52e4b71
zxclovetotti <zxclovetotti@gmail.com>
2016-12-31 10:02:01 +0800

    added slideshow

--- domsters/scripts/global.js
+++ domsters/scripts/global.js
@@ -397,14 +397,18 @@ function resetFields(whichform){
 }
 
 function prepareForms() {
-    for (var i = 0; i < document.forms.length; i++){
+    for (var i=0; i<document.forms.length; i++) {
         var thisform = document.forms[i];
         resetFields(thisform);
-        thisform.onsubmit = function () {
-            return validateForm(this);
+        thisform.onsubmit = function() {
+            if (!validateForm(this)) return false;
+            var article = document.getElementsByTagName('article')[0];
+            if (submitFormWithAjax(this, article)) return false;
+            return true;
         }
     }
 }
+
 
 function isFilled(field){
     if (field.value.replace(' ','').length == 0) return false;
@@ -433,6 +437,73 @@ function validateForm(whichform) {
         }
     }
 }
+
+function getHTTPObject() {
+    if (typeof XMLHttpRequest == "undefined")
+        XMLHttpRequest = function () {
+            try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
+            catch (e) {}
+            try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
+            catch (e) {}
+            try { return new ActiveXObject("Msxml2.XMLHTTP"); }
+            catch (e) {}
+            return false;
+        }
+    return new XMLHttpRequest();
+}
+
+function displayAjaxLoading(element) {
+    // Remove the existing content.
+    while (element.hasChildNodes()) {
+        element.removeChild(element.lastChild);
+    }
+    //  Create a loading image.
+    var content = document.createElement("img");
+    content.setAttribute("src","images/loading.gif");
+    content.setAttribute("alt","Loading...");
+    // Append the loading element.
+    element.appendChild(content);
+}
+
+function submitFormWithAjax( whichform, thetarget ) {
+
+    var request = getHTTPObject();
+    if (!request) { return false; }
+
+    // Display a loading message.
+    displayAjaxLoading(thetarget);
+
+    // Collect the data.
+    var dataParts = [];
+    var element;
+    for (var i=0; i<whichform.elements.length; i++) {
+        element = whichform.elements[i];
+        dataParts[i] = element.name + '=' + encodeURIComponent(element.value);
+    }
+    var data = dataParts.join('&');
+
+    request.open('POST', whichform.getAttribute("action"), true);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+            if (request.status == 200 || request.status == 0) {
+                var matches = request.responseText.match(/<article>([\s\S]+)<\/article>/);
+                if (matches.length > 0) {
+                    thetarget.innerHTML = matches[1];
+                } else {
+                    thetarget.innerHTML = '<p>Oops, there was an error. Sorry.</p>';
+                }
+            } else {
+                thetarget.innerHTML = '<p>' + request.statusText + '</p>';
+            }
+        }
+    };
+
+    request.send(data);
+
+    return true;
+};
 
 // Load events
 addLoadEvent(highlightPage);
