0c5e912a23e3f6cda30a95a16d1c95e05e4e9c6c
zxclovetotti <zxclovetotti@gmail.com>
2016-12-31 09:30:58 +0800

    added validate function and bug fixed

--- domsters/scripts/global.js
+++ domsters/scripts/global.js
@@ -419,13 +419,13 @@ function isEmail(field){
 function validateForm(whichform) {
     for (var i = 0; i < whichform.elements.length; i++){
         var element = whichform.elements[i];
-        if (element.required == 'required'){
+        if (element.getAttribute('required') == 'required'){
             if (!isFilled(element)){
                 alert("Please fill in the "+element.name+" field.");
                 return false;
             }
         }
-        if (element.type == 'email'){
+        if (element.getAttribute('type') == 'email'){
             if (!isEmail(element)) {
                 alert("The " + element.name + " field must be a valid email address.");
                 return false;
